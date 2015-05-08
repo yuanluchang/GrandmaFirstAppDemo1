27f7814a3d8820fc5469e26e64a543ddd4355ae9
Yuan-Lu Chang <yuanluchang@Yuan-LudeMacBook-Air.local>
2015-05-07 17:00:55 -0700

    Initial commit

---
+++ app/src/main/java/com/firstapp/appdemo1/DeveloperKey.java
@@ -0,0 +1,18 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+
+package com.firstapp.appdemo1;
+
+/**
+ * Static container class for holding a reference to your YouTube Developer Key.
+ */
+public class DeveloperKey {
+
+  /**
+   * Please replace this with a valid API key which is enabled for the
+   * YouTube Data API v3 service. Go to the
+   * <a href="https://console.developers.google.com/">Google Developers Console</a>
+   * to register a new developer key.
+   */
+  public static final String DEVELOPER_KEY = "AIzaSyD4HrIpca0nGEG6VVJyO8x5ODH5NeRMecs";
+
+}
